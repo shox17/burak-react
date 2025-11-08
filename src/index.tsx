579f89bca4135cdb5292ea16a4d11703ec6717cb
shox17 <159898318+shox17@users.noreply.github.com>
2025-11-08 13:26:17 +0900

    feat: create routing system

--- src/index.tsx
+++ src/index.tsx
@@ -7,18 +7,22 @@ import reportWebVitals from "./reportWebVitals";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./app/MaterialTheme";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./css/index.css";
 
+// Global Integration
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <Router>
+          <App />
+        </Router>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("root") 
 );
 
 // If you want to start measuring performance in your app, pass a function
