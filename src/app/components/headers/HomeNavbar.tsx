1b0217bf267e53a2af7305c2caf72905c1561db3
shox17 <159898318+shox17@users.noreply.github.com>
2025-11-12 12:02:21 +0900

    fix: modify homeNavbar header component

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -9,10 +9,7 @@ export function HomeNavbar() {
         <Stack className="menu">
           <Box>
             <NavLink to="/">
-              <img
-                className="brand-logo"
-                src="/icons/burak.svg"
-              />
+              <img className="brand-logo" src="/icons/burak.svg" />
             </NavLink>
           </Box>
 
@@ -68,7 +65,25 @@ export function HomeNavbar() {
             )}
           </Stack>
         </Stack>
-        <Stack>DETAIL </Stack>
+        <Stack className={"header-frame"}>
+          <Stack className={"detail"}>
+            <Box className={"head-main-txt"}>
+              World's Most Delicious Cousine
+            </Box>
+            <Box className={"wel-txt"}>The Choice, not just a choice</Box>
+            <Box className={"service-txt"}>24 hours service</Box>
+            <Box className={"signup"}>
+              {!authMember ? (
+                <Button variant={"contained"} className={"signup-button"}>
+                  SIGN UP
+                </Button>
+              ) : null}
+            </Box>
+          </Stack>
+          <Box className={"logo-frame"}>
+            <div className={"logo-img"}></div>
+          </Box>
+        </Stack>
       </Container>
     </div>
   );
