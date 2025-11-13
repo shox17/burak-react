5fd79632e1d64d6d49d4e5d03095411678bbff97
shox17 <159898318+shox17@users.noreply.github.com>
2025-11-13 15:17:46 +0900

    fix: organize homepage related sectional components

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,5 +1,21 @@
 import { Container } from "@mui/material";
+import React from "react";
+import Statistics from "./Statistics";
+import PopularDishes from "./PopularDishes";
+import NewDishes from "./NewDishes";
+import ActiveUsers from "./ActiveUsers";
+import Advertisement from "./Advertisement";
+import Events from "./Events";
 
 export default function HomePage() {
-  return <Container>HomePage</Container>;
+  return (
+    <div className={"homepage"}>
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Advertisement />
+      <ActiveUsers />
+      <Events />
+    </div>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ import "./css/index.css";
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
-// Global Integration
+// Global Integration > REDUX, MUI, ROUTER ...
 root.render(
   <React.StrictMode>
     <Provider store={store}>
