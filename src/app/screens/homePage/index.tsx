13577b79ad7a9fe450f6e1097a10ce707ba47680
shox17 <159898318+shox17@users.noreply.github.com>
2025-11-13 15:40:45 +0900

    fix: modify statistics sectional component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -6,6 +6,7 @@ import NewDishes from "./NewDishes";
 import ActiveUsers from "./ActiveUsers";
 import Advertisement from "./Advertisement";
 import Events from "./Events";
+import "../../../css/home.css";
 
 export default function HomePage() {
   return (
